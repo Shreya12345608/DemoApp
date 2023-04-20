681eb22518f637df79fdb343a779e98e07a65179
AD001\z004efdb <shreya.malviya@siemens.com>
2023-04-20 09:26:13 +0530

    [Shreya]Add- : Demo App

---
+++ DemoApp/DemoApp/App.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoApp
+{
+    class App
+    {
+        //method - function
+        //public access modefier
+        //void- retuen type
+        //add () method name
+        public void Add()
+        {
+            Console.WriteLine("Shreya");
+        }
+    }
+}
